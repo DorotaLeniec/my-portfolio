95aa83d978bbe4c590130261acdd845d48cab222
dorota <dorleniec@gmail.com>
2017-02-02 17:15:04 +0100

    fixing navigation bar

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,9 +1,15 @@
 
 
-$('#navigation-close-button').on('click',function () {
-  $('.navigation-item-link').animate({right: '300px'}, "slow");
+$('.close-button').on('click',function () {
+  $('.navigation-item-link').animate({right: '300px'});
   $('.left-line').css({'transform' : 'rotate(90deg)'});
   $('.additional-line').show().css({'transform' : 'rotate(90deg)'});
   $('.right-line').css({'transform' : 'rotate(-90deg) translate(10px,-6px)'});
+  $('#navigation-close-button').removeClass('close-button');
+  $('#navigation-close-button').addClass('open-button');
+  $('.navigation-background').css({'transform' :  'translate(-220px,-220px)'});
+})
 
+$('body').on('click','open-button', function () {
+  $('.open-button').hide()
 })
