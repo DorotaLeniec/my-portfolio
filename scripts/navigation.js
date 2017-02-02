0fddd34761d3419b36511893df98dc89ef4d1d97
dorota <dorleniec@gmail.com>
2017-02-02 19:56:34 +0100

    navigation bar close and open

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,15 +1,22 @@
 
+var navigation_closed = false;
+
+
 
 $('.close-button').on('click',function () {
-  $('.navigation-item-link').animate({right: '300px'});
-  $('.left-line').css({'transform' : 'rotate(90deg)'});
-  $('.additional-line').show().css({'transform' : 'rotate(90deg)'});
-  $('.right-line').css({'transform' : 'rotate(-90deg) translate(10px,-6px)'});
-  $('#navigation-close-button').removeClass('close-button');
-  $('#navigation-close-button').addClass('open-button');
-  $('.navigation-background').css({'transform' :  'translate(-220px,-220px)'});
-})
-
-$('body').on('click','open-button', function () {
-  $('.open-button').hide()
+  if(navigation_closed){
+    $('.left-line').css({'transform' : 'rotate(45deg)'});
+    $('.right-line').css({'transform' : 'rotate(-45deg) translate(0px,0px)'});
+    $('.additional-line').hide();
+    $('.navigation-background').css({'transform' :  'translate(0px,0px)'});
+    $('.navigation-item-link').animate({right: '0'}, 'slow');
+    navigation_closed=false;
+  } else {
+      $('.navigation-item-link').animate({right: '300px'});
+      $('.left-line').css({'transform' : 'rotate(90deg)'});
+      $('.additional-line').show().css({'transform' : 'rotate(90deg)'});
+      $('.right-line').css({'transform' : 'rotate(-90deg) translate(10px,-6px)'});
+      $('.navigation-background').css({'transform' :  'translate(-220px,-220px)'});
+      navigation_closed=true;
+    }
 })
