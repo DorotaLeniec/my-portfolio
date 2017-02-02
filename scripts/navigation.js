3d62ba7a2aa0b2618e915dd8e02a1f65a9d388ae
dorota <dorleniec@gmail.com>
2017-02-02 16:54:54 +0100

    close button animates into hamburger

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,4 +1,9 @@
 
+
 $('#navigation-close-button').on('click',function () {
-  $('#navigation-container').fadeOut();
+  $('.navigation-item-link').animate({right: '300px'}, "slow");
+  $('.left-line').css({'transform' : 'rotate(90deg)'});
+  $('.additional-line').show().css({'transform' : 'rotate(90deg)'});
+  $('.right-line').css({'transform' : 'rotate(-90deg) translate(10px,-6px)'});
+
 })
